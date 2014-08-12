64f077c47c64b90d148b579804bca1373f6ead0f
shaunhend <shaun.henderson@fifthring.com>
2014-08-12 15:36:42 +0100

    Update filesystem.php

--- src/config/filesystem.php
+++ src/config/filesystem.php
@@ -14,7 +14,7 @@ return [
 	|
 	*/
 
-	'url' => '/system/:class/:attachment/:id_partition/:style/:filename',
+	'url' => '/:attachment/:style/:filename',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -28,7 +28,7 @@ return [
 	|
 	*/
 
-	'path' => ':app_root/public:url',
+	'path' => ':app_root/web:url',
 
 	/*
 	|--------------------------------------------------------------------------
